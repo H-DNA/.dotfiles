10dd6d65f4f62b645e1c26b518610e32317e99a6
Huy-DNA <huydo862003@gmail.com>
2024-10-31 16:41:13 +0700

    feat: show gitignored files

--- .config/nvim/lua/core/plugin_configs/nvim-tree.lua
+++ .config/nvim/lua/core/plugin_configs/nvim-tree.lua
@@ -1,6 +1,13 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  update_cwd = true,
+  git = {
+    enable = true,
+    ignore = false,
+    timeout = 500,
+  },
+})
 
 vim.keymap.set("n", "<c-n>", ":NvimTreeFindFileToggle<CR>")
