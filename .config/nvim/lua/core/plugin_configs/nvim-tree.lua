12cfcfde416e46ecb1a66abdc1e5bfc313ab512d
Huy-DNA <huydo862003@gmail.com>
2024-11-13 01:25:43 +0700

    feat: close nvim-tree on closing a file + various utilities

--- .config/nvim/lua/core/plugin_configs/nvim-tree.lua
+++ .config/nvim/lua/core/plugin_configs/nvim-tree.lua
@@ -8,11 +8,37 @@ require("nvim-tree").setup({
     number = true,
     relativenumber = true,
   },
+  renderer = {
+    add_trailing = true,
+    highlight_opened_files = "all",
+    highlight_modified = "all",
+    indent_markers = {
+      enable = true,
+    }
+  },
   update_cwd = true,
+  update_focused_file = {
+    enable = true,
+  },
   git = {
     enable = true,
     ignore = false,
     timeout = 500,
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+  },
+  modified = {
+    enable = true,
+  },
+  filters = {
+    enable = false,
+  },
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
   },
 })
 
