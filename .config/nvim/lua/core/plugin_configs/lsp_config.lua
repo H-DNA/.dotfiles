93092163c08567b5b04f1e3700fe51fc665baf0c
Huy-DNA <huydo862003@gmail.com>
2024-11-13 01:31:12 +0700

    fix: refactor lsp_config

--- .config/nvim/lua/core/plugin_configs/lsp_config.lua
+++ .config/nvim/lua/core/plugin_configs/lsp_config.lua
@@ -1,25 +1,27 @@
+local lsps = {
+  "lua_ls",
+  "rust_analyzer",
+  "clangd",
+  "cssls",
+  "dockerls",
+  "eslint",
+  "html",
+  "marksman",
+  "tailwindcss",
+  "tsserver",
+  "jsonls",
+  "pyright",
+  "gopls",
+  "golangci_lint_ls",
+  "volar",
+  "volar",
+  "sqls",
+  "elp",
+}
+
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = {
-    "lua_ls",
-    "rust_analyzer",
-    "clangd",
-    "cssls",
-    "dockerls",
-    "eslint",
-    "html",
-    "marksman",
-    "tailwindcss",
-    "tsserver",
-    "jsonls",
-    "pyright",
-    "gopls",
-    "golangci_lint_ls",
-    "volar",
-    "volar",
-    "sqls",
-    "elp",
-  },
+  ensure_installed = lsps,
 })
 local on_attach = function(_, _)
   vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
@@ -28,94 +30,13 @@ local on_attach = function(_, _)
   vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, {})
   vim.keymap.set("n", "<leader>gh", vim.lsp.buf.hover, {})
   vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  vim.diagnostic.config({ update_in_insert = true })
 end
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-local lspconfig = require("lspconfig")
-
-lspconfig.sqls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.volar.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.solargraph.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.elp.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.lua_ls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.rust_analyzer.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.clangd.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.cssls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.dockerls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.html.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.jsonls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.marksman.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.pyright.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.sorbet.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.rubocop.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.tailwindcss.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.tsserver.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.volar.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.gopls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.golangci_lint_ls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-lspconfig.eslint.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
-vim.diagnostic.config({ update_in_insert = true })
+for index = 1, #lsps do
+  local lsp_name = lsps[index]
+  require("lspconfig")[lsp_name].setup({
+    on_attach = on_attach,
+    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+  })
+end
