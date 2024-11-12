81da6008bfb0ab7fb1203faa66e1a9c4eb7abaed
Huy-DNA <huydo862003@gmail.com>
2024-11-13 02:59:44 +0700

    feat: extend telescope

--- .config/nvim/lua/core/plugin_configs/telescope.lua
+++ .config/nvim/lua/core/plugin_configs/telescope.lua
@@ -1,6 +1,11 @@
 local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<Space>ff", builtin.find_files, {})
-vim.keymap.set("n", "<Space>fh", builtin.oldfiles, {})
 vim.keymap.set("n", "<Space>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<Space>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<Space>fw", builtin.grep_string, {})
+vim.keymap.set("n", "<Space>fs", builtin.treesitter, {})
+vim.keymap.set("n", "<Space>fc", builtin.git_commits, {})
+vim.keymap.set("n", "<Space>fb", builtin.git_branches, {})
+vim.keymap.set("n", "<Space>ft", builtin.git_status, {})
+vim.keymap.set("n", "<Space>fm", builtin.man_pages, {})
