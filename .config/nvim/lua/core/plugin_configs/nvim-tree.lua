4d2f19946bea3d2ba9509979652da28c4a061739
Huy-DNA <huydo862003@gmail.com>
2024-11-13 01:07:09 +0700

    feat: improve nvim tree with options

--- .config/nvim/lua/core/plugin_configs/nvim-tree.lua
+++ .config/nvim/lua/core/plugin_configs/nvim-tree.lua
@@ -2,6 +2,12 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
+  disable_netrw = true,
+  hijack_cursor = true,
+  view = {
+    number = true,
+    relativenumber = true,
+  },
   update_cwd = true,
   git = {
     enable = true,
