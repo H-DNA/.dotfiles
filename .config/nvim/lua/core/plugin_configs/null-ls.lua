d72eafff954b8df1803042a4fb13a77901850ea1
Huy-DNA <huydo862003@gmail.com>
2024-11-21 11:02:32 +0700

    fix: ensure install eslint

--- .config/nvim/lua/core/plugin_configs/null-ls.lua
+++ .config/nvim/lua/core/plugin_configs/null-ls.lua
@@ -1,12 +1,12 @@
 require("mason-null-ls").setup({
-  ensure_installed = { "stylua", "jq" },
+  ensure_installed = { "jq", "eslint" },
 })
 
 local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.eslint,
     null_ls.builtins.diagnostics.eslint,
   },
 })
