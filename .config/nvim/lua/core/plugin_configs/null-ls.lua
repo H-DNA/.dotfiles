2c7c329ecd73df22e5e4b00f10aa1e0a4455655d
Huy-DNA <huydo862003@gmail.com>
2024-11-21 11:28:52 +0700

    fix: simplfy null-ls config

--- .config/nvim/lua/core/plugin_configs/null-ls.lua
+++ .config/nvim/lua/core/plugin_configs/null-ls.lua
@@ -1,12 +1,11 @@
 require("mason-null-ls").setup({
-  ensure_installed = { "jq", "eslint" },
+  ensure_installed = { "jq", "stylua" },
 })
 
 local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.eslint,
-    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.formatting.stylua,
   },
 })
