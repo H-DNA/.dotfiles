5d2b94fda99f8c21f1ed39c53c51598cef1952a6
HuyDNA <huydo862003@gmail.com>
2025-01-06 14:34:21 +0700

    fix: prefix lsp key binds with `l`

--- .config/nvim/lua/plugins/configs/language_services/lsp/init.lua
+++ .config/nvim/lua/plugins/configs/language_services/lsp/init.lua
@@ -1,19 +1,19 @@
 -- lsp keymaps
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-vim.keymap.set("n", "<leader>gi", vim.lsp.buf.references, {})
-vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, {})
-vim.keymap.set("n", "<leader>gh", vim.lsp.buf.hover, {})
-vim.keymap.set("n", "<leader>gf", function()
+vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, {})
+vim.keymap.set("n", "<leader>li", vim.lsp.buf.references, {})
+vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, {})
+vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "<leader>lf", function()
   if vim.fn.exists(':EslintFixAll') ~= 0 then
     vim.cmd('EslintFixAll')
   else
     vim.lsp.buf.format({ async = true })
   end
 end, {})
-vim.keymap.set("n", "<leader>gs", vim.lsp.buf.document_symbol, {})
-vim.keymap.set("n", "<leader>gc", vim.lsp.buf.outgoing_calls, {})
-vim.keymap.set("n", "<leader>go", vim.diagnostic.open_float, {})
+vim.keymap.set("n", "<leader>ls", vim.lsp.buf.document_symbol, {})
+vim.keymap.set("n", "<leader>lc", vim.lsp.buf.outgoing_calls, {})
+vim.keymap.set("n", "<leader>lo", vim.diagnostic.open_float, {})
 
 vim.diagnostic.config({ update_in_insert = true })
 
