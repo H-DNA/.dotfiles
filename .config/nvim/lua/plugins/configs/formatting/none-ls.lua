e9452d4fd218ee1cc7553e92700fbc8a5e36425e
Huy-DNA <huydo862003@gmail.com>
2026-01-07 17:34:50 +0700

    fix: js format bug in nvim plugin

--- .config/nvim/lua/plugins/configs/formatting/none-ls.lua
+++ .config/nvim/lua/plugins/configs/formatting/none-ls.lua
@@ -13,14 +13,4 @@ null_ls.setup({
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.gofumpt,
   },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
 })
